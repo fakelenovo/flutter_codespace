f8dc060c2f289a6f4e70c6df22eb4e459cc3ad9f
Podvalchik <podvalchik69@gmail.com>
2023-01-22 13:40:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_codespace
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_codespace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_codespace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
